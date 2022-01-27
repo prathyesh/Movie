aa353210f3db16cf9c4ccf39315c16dc433e4d08
Prathyesh <prathyeshasdf@gmail.com>
2022-01-27 18:27:58 +0530

    This is my first commit

--- src/App.js
+++ src/App.js
@@ -1,55 +1,85 @@
-import React, { Component } from 'react'
-import FetchData from './Components/FetchData'
+import React, { Component } from 'react';
+import FetchData from './Components/FetchData';
 import CustomPagination from './Components/CustomPagination/CustomPagination';
 import CircularProgress from '@mui/material/CircularProgress';
-
-import "./App.css"
+import NavBar from './Components/NavBar';
+import './App.css';
 
 export class App extends Component {
-  constructor(props){
-    super(props);
-    this.state={Mdata:[],
-    CurrentPage:1
-};
-}
+	constructor(props) {
+		super(props);
+		this.state = {
+			type:'all',
+			CurrentPage:1,
+			Mdata: []
+		};
+	}
 
-setPage=(page)=>{
-  this.setState({CurrentPage:page},()=>{
-    this.FetchData_fetch();
-  });
-}
-async FetchData_fetch(){
-  const ans=await fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=b341f65f7831bca4d2c7dae31d4194db&page=${this.state.CurrentPage}`);
-  const { results }=await ans.json();
-  console.log(`https://api.themoviedb.org/3/trending/all/day?api_key=b341f65f7831bca4d2c7dae31d4194db&page=${this.state.CurrentPage}`);
-  console.log(results);
-  this.setState({Mdata:results});
-}
-componentDidMount(){
-    this.FetchData_fetch();
-}
-  render() {
-    return (
-      <div>
-        <h1 style={{position:"sticky"}} className='d-flex justify-content-center' onClick={()=>{window.scroll(0,0)}} >Treanding</h1>
-        <div className='App container'>
-          {
-            this.state.Mdata.length?
-          this.state.Mdata.map(ele =>{
-          return <FetchData
-              key={ele.id}
-              pic={`https://image.tmdb.org/t/p/w300${ele.poster_path}`}
-              name={ele.title || ele.name}
-              date={ele.release_date || ele.first_air_date}
-              type={ele.media_type}
-              rating={ele.vote_average}
-              />;
-          }):<CircularProgress />}
-          <CustomPagination setPage={this.setPage} page={this.CurrentPage}/>
-        </div>
-      </div>
-    )
+  changeType=(e)=>{
+	  console.log(e);
+	  if(e==="Trending"){
+		  e="all"
+	  }else if(e==="Movies"){
+		  e="movie"
+	  }else{
+		  e="tv"
+	  }
+	this.setState({type:e,CurrentPage:1},()=>{this.FetchData_fetch()});
   }
+
+	setPage = (page) => {
+		this.setState({ CurrentPage: page }, () => {
+			this.FetchData_fetch();
+		});
+	};
+	async FetchData_fetch() {
+		const ans = await fetch(
+			`https://api.themoviedb.org/3/trending/${this.state.type}/day?api_key=b341f65f7831bca4d2c7dae31d4194db&page=${this.state
+				.CurrentPage}`
+		);
+		const { results } = await ans.json();	
+		console.log(results);	
+		this.setState({Mdata:results});
+	}
+	componentDidMount() {
+		this.FetchData_fetch();
+	}
+	render() {
+		return (
+			<div>
+				{/* <ResponsiveAppBar/> */}
+				<NavBar fun={this.changeType}/>
+				<h1
+					style={{fontFamily: 'Rajdhani'}}
+					className="d-flex justify-content-center"
+					onClick={() => {
+						window.scroll(0, 0);
+					}}
+				>
+					{this.state.type==="all"?"Trending":this.state.type==="movie"?"Movies":"Tv Shows"}
+				</h1>
+				<div className="App container">
+					{this.state.Mdata.length ? (
+						this.state.Mdata.map((ele) => {
+							return (
+								<FetchData
+									key={ele.id}
+									pic={`https://image.tmdb.org/t/p/w300${ele.poster_path}`}
+									name={ele.title || ele.name}
+									date={ele.release_date || ele.first_air_date}
+									type={ele.media_type}
+									rating={ele.vote_average}
+								/>
+							);
+						})
+					) : (
+						<CircularProgress />
+					)}
+					<CustomPagination setPage={this.setPage} page={this.CurrentPage} />
+				</div>
+			</div>
+		);
+	}
 }
 
-export default App
+export default App;
---
+++ src/Components/NavBar/index.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import "./sample.css";
+
+export class NavBar extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { windowWidth: window.innerWidth ,toggle:false,SearchData:[]};
+		const Search="";
+	  }
+	  handleResize = (e) => {
+		this.setState({ windowWidth: window.innerWidth });
+	   };
+	  
+	   componentDidMount() {
+		window.addEventListener("resize", this.handleResize);
+	   }
+	  
+	   componentWillUnmount() {
+		window.addEventListener("resize", this.handleResize);
+	   } 
+	HandleChange=(e)=>{
+		this.Search=e.target.value;
+		if(this.Search.length){
+		this.Fetch_Search();
+		}
+	}
+	HandleSearch=()=>{
+		console.log(this.state.SearchData.length);
+		// this.Fetch_Search();
+	}
+    HandleClick=(e)=>{
+        this.props.fun(e.target.textContent);
+    }
+	HandleToggle=()=>{
+		this.setState({toggle:this.state.toggle?false:true});
+	}
+	async Fetch_Search(){
+		const req=await fetch(`https://api.themoviedb.org/3/search/company?api_key=b341f65f7831bca4d2c7dae31d4194db&query=${this.Search}`);
+		const res=await req.json();
+		this.setState({SearchData:res.results});
+	}
+	render() {
+		const IsToggled=this.state.toggle;
+		const Width=this.state.windowWidth;
+		return (
+			<>
+				<div className="Movie-Container">
+					<div className="Movie-Heading">
+					<div className="Heading">
+						<h1>GoGoMovies</h1>
+					</div>
+					<div className="Bar-Btn">
+						<button className="Hbar" onClick={this.HandleToggle}><i className="fas fa-bars" /></button>
+					</div>
+				</div>
+						{
+							(Width>992 || IsToggled)?
+								<>
+								    <div className="Movie-Pages">
+										<div className>
+											<p onClick={this.HandleClick}>Trending</p>
+										</div>
+										<div className>
+											<p onClick={this.HandleClick}>Movies</p>
+										</div>
+										<div className>
+											<p onClick={this.HandleClick}>Tv Shows</p>
+										</div>
+										</div>
+										<div className="Movie-Search-op">
+										<div className="For-Pushing-Right">
+											<input className="Submit-St" onChange={this.HandleChange} placeholder="Search" type="search" />
+											<button className="Btn-Custome" onClick={this.HandleSearch} type="submit">Search</button>
+										</div>
+									</div>
+								</>:<></>
+						}
+					</div>
+			</	>
+		);
+	}
+}
+export default NavBar;
